d751338ba8776a77d1fb90ac0e1ead2caa02319b
Navneet Prajapati <navneetprajapati26@gmail.com>
2023-10-09 19:01:59 +0530

    3rd commit

--- test/http_utils_test.dart
+++ test/http_utils_test.dart
@@ -1,24 +1,29 @@
+
+import 'package:flutter_test/flutter_test.dart';
 import 'package:utility_package/http_utils.dart';
 
-void main() async {
-  final apiRepo = HttpOperationsRepository(
-    'https://jsonplaceholder.typicode.com/',
-    defaultHeaders: {'Content-Type': 'application/json'},
-    timeoutDuration: Duration(seconds: 5),
-  );
+void main() {
+  HttpOperationsRepository? apiRepo;
 
-  // Example usages:
-  try {
-    final post = await apiRepo.get('posts/1');
-    //print(post);
+  setUp(() {
+    apiRepo = HttpOperationsRepository(
+      'https://jsonplaceholder.typicode.com/',
+      defaultHeaders: {'Content-Type': 'application/json'},
+      timeoutDuration: Duration(seconds: 5),
+    );
+  });
 
-    final newPost = await apiRepo.post('posts', body: {
+  test('GET request', () async {
+    final post = await apiRepo!.get('posts/1');
+    expect(post, isNotNull);
+  });
+
+  test('POST request', () async {
+    final newPost = await apiRepo!.post('posts', body: {
       'title': 'foo',
       'body': 'bar',
       'userId': 1,
     });
-   // print(newPost);
-  } catch (e) {
-    //print('Error: $e');
-  }
+    expect(newPost, isNotNull);
+  });
 }
